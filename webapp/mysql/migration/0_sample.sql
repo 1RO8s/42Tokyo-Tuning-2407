3f5686ff45b51afe400b6fe8648785005cdc74dc
1RO8s <saki0220hiro@gmail.com>
2024-07-27 18:43:23 +0900

    DBインデックス追加

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,1 +1,4 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
+ALTER TABLE users ADD INDEX idx_username (username);
+ALTER TABLE sessions ADD INDEX idx_session_token (session_token);
+ALTER TABLE locations ADD INDEX idx_locations_tow_truck_id_timestamp (tow_truck_id, timestamp);
